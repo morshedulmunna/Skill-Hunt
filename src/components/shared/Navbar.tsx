aef39c88d156f3a47ca43e554b99759a9a717d55
morshedulmunna <morshedulmunna1@gmail.com>
2024-12-05 04:44:57 +0600

    buy set button

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -44,12 +44,12 @@ export default function Navbar(): JSX.Element {
               >
                 Sign in
               </Link>
-              <Link
-                href={"/signin"}
+              <button
+                type="button"
                 className="px-4 py-2 rounded bg-primary-base whitespace-nowrap  text-white  focus:outline-none "
               >
                 Post a Jobs
-              </Link>
+              </button>
               <ThemeToggler />
             </div>
           </div>
