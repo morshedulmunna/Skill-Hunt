4dd22166ecaade1302e3fc0347e2f7129f34aa3e
morshedulmunna <morshedulmunna1@gmail.com>
2024-12-04 17:11:23 +0600

    logo component

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,3 +1,4 @@
+import LogoSvgIcon from "@/assets/svg/LogoSvgIcon";
 import Link from "next/link";
 import React from "react";
 
@@ -8,8 +9,11 @@ interface LogoProps {
 const Logo: React.FC<LogoProps> = ({ className, ...props }) => {
   return (
     <div className={`${className}`} {...props}>
-      <Link className="font-semibold text-xl " href={"/"}>
-        Skill Hunt
+      <Link
+        className="font-semibold flex items-center gap-2 text-xl "
+        href={"/"}
+      >
+        <LogoSvgIcon /> SH
       </Link>
     </div>
   );
