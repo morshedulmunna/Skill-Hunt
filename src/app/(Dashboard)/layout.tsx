dd28287476e12e2f838da71acdbe61e142e4bf50
morshedulmunna <morshedulmunna1@gmail.com>
2024-12-06 00:12:04 +0600

    dashboard layout

--- src/app/(Dashboard)/layout.tsx
+++ src/app/(Dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import DashboardSidebar from "@/components/DashboardSidebar";
+import MaxWidthWrapper from "@/components/shared/MaxWidthWrapper";
 import React from "react";
 
 type Props = {
@@ -5,5 +7,16 @@ type Props = {
 };
 // ---
 export default function DashboardLayout({ children }: Props) {
-  return <>{children}</>;
+  return (
+    <>
+      <MaxWidthWrapper className="w-full h-full">
+        <div className="grid gap-4 grid-cols-12 h-full">
+          <div className="border-r dark:border-gray-800 col-span-3 h-full">
+            <DashboardSidebar />
+          </div>
+          <div className="col-span-9 p-4 h-full">{children}</div>
+        </div>
+      </MaxWidthWrapper>
+    </>
+  );
 }
---
+++ src/components/DashboardSidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+export default function DashboardSidebar({}: Props) {
+  return (
+    <div className="p-4">
+      <div>Dashboard </div>
+    </div>
+  );
+}
